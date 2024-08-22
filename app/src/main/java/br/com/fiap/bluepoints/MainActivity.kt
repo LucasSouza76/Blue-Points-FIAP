e25513f54c2522d56912df01b01d99f707bfdd7e
LucasSouza76 <lucassouzajose@gmail.com>
2024-08-22 14:49:14 -0300

    Criação da tela de primeiro acesso e config de cores e fonts

--- app/src/main/java/br/com/fiap/bluepoints/MainActivity.kt
+++ app/src/main/java/br/com/fiap/bluepoints/MainActivity.kt
@@ -4,13 +4,39 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.fontResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.com.fiap.bluepoints.ui.theme.BluePointsTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +45,94 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BluePointsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "BluePoints",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Login()
             }
         }
     }
 }
 
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Login() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color.White
+    ) {
+        Box(
+            modifier = Modifier.padding(20.dp),
+            contentAlignment = Alignment.CenterStart
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Row(
+                     modifier = Modifier.padding(top = 40.dp),
+                    ) {
+                    Text(
+                        text = "Blue",
+                        fontSize = 45.sp,
+                        fontWeight = FontWeight.Medium,
+                        color = colorResource(id = R.color.azul_primario)
+                    )
+                    Text(
+                        text = "Points",
+                        fontSize = 45.sp,
+                        fontWeight = FontWeight.Medium,
+                        color = Color.Black
+                    )
+                }
+            }
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(400.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.img_login),
+                    contentDescription ="img_login",
+                    modifier = Modifier.height(400.dp)
+                )
+            }
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.BottomCenter),
+                verticalArrangement = Arrangement.spacedBy(20.dp)
+            ) {
+               ElevatedButton(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .height(44.dp)
+                        .fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(containerColor = colorResource(id = R.color.azul_primario))
+                ) {
+                    Text(
+                        text = "Login",
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Medium,
+                    )
+                }
+               ElevatedButton(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .height(44.dp)
+                        .fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(containerColor = colorResource(id = R.color.azul_secundario))
+                ) {
+                    Text(
+                        text = "Cadastre-se",
+                        color = colorResource(id = R.color.azul_primario),
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Medium,
+                    )
+                }
+            }
+        }
+    }
+
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BluePointsTheme {
-        Greeting("BluePoints")
-    }
-}
+
